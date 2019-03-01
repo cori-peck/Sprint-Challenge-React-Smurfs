90e7f1f4463fbe2e3b5096398c1c387244e7912b
cori-peck <cori.peck@ymail.com>
2019-03-01 13:04:22 -0600

    Project as of Times-Up: added more styles, didn't get to delete

--- village/src/components/Smurf.js
+++ village/src/components/Smurf.js
@@ -1,11 +1,16 @@
 import React from 'react';
 
+import './Smurf.css';
+
 const Smurf = props => {
   return (
     <div className="Smurf">
-      <h3>{props.name}</h3>
-      <strong>{props.height} cm tall</strong>
-      <p>{props.age} smurf years old</p>
+      <div className="card-container">
+          <h3 className="cardTitle">{props.name}</h3>
+          <img className="cardImg" src="https://vignette.wikia.nocookie.net/smurfs/images/0/07/Smurf_Village_Comic_Books.jpg/revision/latest?cb=20120417231142" alt="Smurf Village" />
+          <strong className="cardText">{props.height} cm tall</strong>
+          <p className="cardText">{props.age} smurf years old</p>
+        </div>
     </div>
   );
 };
